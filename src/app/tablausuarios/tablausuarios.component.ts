da3bb471d0e7cb902d98f666abdab1dd882c18e4
Rafael de Haro <rdharooeig@gmail.com>
2024-04-16 15:49:24 +0200

    dia 2

---
+++ src/app/tablausuarios/tablausuarios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tablausuarios',
+  templateUrl: './tablausuarios.component.html',
+  styleUrl: './tablausuarios.component.css'
+})
+export class TablausuariosComponent {
+
+}
