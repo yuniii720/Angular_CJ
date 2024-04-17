48c8f119539acb668ec6d46dae19ceb6cec0bab8
Rafael de Haro <rdharooeig@gmail.com>
2024-04-17 08:42:04 +0200

    botones

---
+++ src/app/botonestabla/botonestabla.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botonestabla',
+  template : `
+  <input type="button" value="Editar" class="button-4"><input type="button" class="button-4" value="Borrar">
+  `,
+  styleUrl: './botonestabla.component.css'
+})
+export class BotonestablaComponent {
+
+}
