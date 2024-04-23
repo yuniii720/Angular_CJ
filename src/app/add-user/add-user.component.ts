133b076017fcc7cf4dd02bc1f7da30307fc4c868
joshb3nz <josh.fm14@gmail.com>
2024-04-23 08:42:29 +0200

    Mejora AddUser Modal

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,11 +1,35 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrl: './add-user.component.css'
+  styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  userForm: FormGroup;
+
+  constructor(public dialogRef: MatDialogRef<AddUserComponent>) {
+    // Inicializa el formulario aquí
+    this.userForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      name: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      type: new FormControl(''),
+      created_at: new FormControl({ value: new Date().toISOString(), disabled: true }),
+    });
+  }
+
+  onSubmit(): void {
+    // Cierra el modal y pasa los datos del formulario al componente que lo abrió
+    if (this.userForm.valid) {
+      this.dialogRef.close(this.userForm.getRawValue());
+    }
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,10 +15,14 @@ import { BtnAddClientComponent } from './btn-add-client/btn-add-client.component
 import { TablaClientesComponent } from './tablaclientes/tablaclientes.component';
 import { TablacuentasComponent } from './tablacuentas/tablacuentas.component';
 import { TablatarjetasComponent } from './tablatarjetas/tablatarjetas.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatDialogModule } from '@angular/material/dialog';
 import { RegisterComponent } from './register/register.component';
 import { EmailserviceService } from './emailservice.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -35,21 +39,22 @@ import { EmailserviceService } from './emailservice.service';
     TablaClientesComponent,
     TablacuentasComponent,
     TablatarjetasComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatDialogModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    HttpClientModule,
-    EmailserviceService
+    EmailserviceService,
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
