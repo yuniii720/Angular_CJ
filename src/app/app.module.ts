992377e9dc9b002e499a8721b556420e1df1ae51
Rafael de Haro <rdharooeig@gmail.com>
2024-04-24 09:16:06 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -16,9 +15,20 @@ import { BtnAddClientComponent } from './components/buttons/btn-add-client/btn-a
 import { TablaClientesComponent } from './components/tables/tablaclientes/tablaclientes.component';
 import { TablacuentasComponent } from './components/tables/tablacuentas/tablacuentas.component';
 import { TablatarjetasComponent } from './components/tables/tablatarjetas/tablatarjetas.component';
-import { RegisterComponent } from './components/register/register.component';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BtnModUserComponent } from './components/buttons/btn-mod-user/btn-mod-user.component';
+import { BtnDelUserComponent } from './components/buttons/btn-del-user/btn-del-user.component';
+import { ModUserComponent } from './components/modals/usuarios/mod-user/mod-user.component';
+import { DelUserComponent } from './components/modals/usuarios/del-user/del-user.component';
+import { ConfirmDialogComponent } from './components/modals/confirm-dialog/confirm-dialog.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,15 +45,26 @@ import { MatDialogModule } from '@angular/material/dialog';
     TablacuentasComponent,
     TablatarjetasComponent,
     RegisterComponent,
+    BtnModUserComponent,
+    BtnDelUserComponent,
+    ModUserComponent,
+    DelUserComponent,
+    ConfirmDialogComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule, 
     HttpClientModule,
     MatDialogModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
