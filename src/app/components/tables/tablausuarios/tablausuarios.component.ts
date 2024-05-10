811a8c14569d3155482ca0b7e702dda9a499d5fb
joshb3nz <josh.fm14@gmail.com>
2024-05-10 13:43:24 +0200

    Mensajes validación Fix

--- src/app/components/tables/tablausuarios/tablausuarios.component.ts
+++ src/app/components/tables/tablausuarios/tablausuarios.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SupabaseService } from '../../../services/supabase.service';
 import { Usuario } from '../../../models/usuario.model';
-import { EventService } from '../../../services/event.service'; // Importa el EventService
 
 @Component({
   selector: 'app-tabla-usuarios',
@@ -13,25 +12,17 @@ export class TablaUsuariosComponent implements OnInit, OnDestroy {
   usuarios: Usuario[] = [];
   private subs = new Subscription();
   displayedColumns: string[] = ['id', 'username', 'name', 'email', 'type', 'created_at', 'gestionar'];
+  usuario: any;
 
-  constructor(
-    private supabaseService: SupabaseService,
-    private eventService: EventService 
-  ) {}
+  constructor(private supabaseService: SupabaseService) { }
 
   ngOnInit(): void {
     this.subs.add(this.supabaseService.usuarios$.subscribe(usuarios => {
       this.usuarios = usuarios;
-    }));
-
-    this.subs.add(this.eventService.confirmEvent$.subscribe(() => {
-      this.mostrarPopup(); 
     }));
   }
 
   ngOnDestroy(): void {
     this.subs.unsubscribe();
   }
-
-  mostrarPopup() {}
 }
