a0092de3dcc0745223f1e32aa0afcef2e5c8907c
Rafael de Haro <rdharooeig@gmail.com>
2024-05-15 13:00:16 +0200

    123321

--- src/app/components/tarjetavisual/tarjetavisual.component.ts
+++ src/app/components/tarjetavisual/tarjetavisual.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core'; // Importar Inject
 import { SupabaseService } from '../../services/supabase.service';
-import { Cliente } from '../../models/cliente.model';
-import { EventService } from '../../services/event.service';
+import { Tarjeta } from '../../models/tarjeta.model';
 
 @Component({
   selector: 'app-tarjetavisual',
   templateUrl: './tarjetavisual.component.html',
-  styleUrl: './tarjetavisual.component.scss'
+  styleUrls: ['./tarjetavisual.component.scss']
 })
-export class TarjetavisualComponent {
+export class TarjetavisualComponent implements OnInit {
+  tarjetas: Tarjeta[] = [];
 
+  constructor(@Inject(SupabaseService) private supabaseService: SupabaseService) {} // Usar @Inject
+
+  ngOnInit(): void {
+    this.cargarTarjetas();
+  }
+
+  cargarTarjetas() {
+    this.supabaseService.tarjeta$.subscribe((tarjetas: Tarjeta[]) => { // Especificar el tipo de tarjetas
+      this.tarjetas = tarjetas;
+    });
+  }
+
+  async eliminarTarjeta(id: number) {
+    try {
+      const confirmar = confirm('¿Estás seguro de que deseas eliminar esta tarjeta?');
+      if (confirmar) {
+        await this.supabaseService.deleteTarjeta(id);
+      }
+    } catch (error) {
+      console.error('Error al eliminar la tarjeta', error);
+    }
+  }
 }
--- src/app/models/tarjeta.model.ts
+++ src/app/models/tarjeta.model.ts
@@ -1,9 +1,12 @@
 export interface Tarjeta {
-  id: string;
   cardNumber: string;
   saldo: number;
   cardHolderName: string;
   expirationDate: string;
   securityCode: string;
   cardType: string;
+}
+
+export interface SaveResult {
+  error?: { message: string };
 }
