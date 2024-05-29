28582df1c2ad4d5364981eda7f4edd74dc44020c
Rafael de Haro <rdharooeig@gmail.com>
2024-05-29 09:29:40 +0200

    123321

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,8 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -35,20 +37,20 @@ export class LoginComponent {
       const res = await this.auth.signIn(email, password);
       console.log('Respuesta del inicio de sesión:', res);
       if (res.user) {
-    
-        'Usuario correcto';
+        this.alertService.success('Inicio de sesión exitoso');
       } else if (res.error) {
-        this.errorMessage = res.error.message || 'Fallo al autenticar usuario.';
+        this.alertService.error(`Error al iniciar sesión: ${res.error.message || res.error}`);
       } else {
-        this.errorMessage = 'Fallo al autenticar usuario.';
+        this.alertService.warning('Recuerda que la entrada a está página sin consentimiento se considera delito y está penado');
       }
     } catch (err: any) {
       console.error('Error durante el inicio de sesión:', err);
       if (err.message.includes("lock")) {
         this.errorMessage = 'Error de bloqueo durante el inicio de sesión. Por favor, inténtelo de nuevo.';
       } else {
-        this.errorMessage = 'Ocurrió un error durante el inicio de sesión: ' + (err.message || err);
+        this.errorMessage = 'Error al iniciar sesión: ' + (err.message || err);
       }
+      this.alertService.error(this.errorMessage);
     }
   }
 }
