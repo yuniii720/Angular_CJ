521a214ad331ed495c8ad9a022df61805fec2465
joshb3nz <josh.fm14@gmail.com>
2024-06-07 12:40:24 +0200

    Main Fix

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrls: ['./main.component.css']
 })
 export class MainComponent {
-
-    }
-  
-  
+  cards = [
+    { balance: '$147,000.00', type: 'VISA', number: '**** **** **** 4782', holder: 'Jamshidbek Tashpulatov', expires: '01/21' },
+    { balance: '$147,000.00', type: 'VISA', number: '**** **** **** 4782', holder: 'Jamshidbek Tashpulatov', expires: '01/21' },
+    { balance: '$147,000.00', type: 'VISA', number: '**** **** **** 4782', holder: 'Jamshidbek Tashpulatov', expires: '01/21' },
+    { balance: '$147,000.00', type: 'VISA', number: '**** **** **** 4782', holder: 'Jamshidbek Tashpulatov', expires: '01/21' }
+    // Add more cards as needed
+  ];
+}
